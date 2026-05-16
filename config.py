d91fbce0935a77bbf3482e29fa6a68471e053871
monsterr0730 <bgmi83071@gmail.com>
2026-05-16 22:51:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Bot Configuration
+BOT_TOKEN = "8466296023:AAF98OCsdXnaN2x6CMEQN3L4TBxpvkUI2pM"
+OWNER_ID = 7192516189
+
+# MongoDB Configuration
+MONGODB_URI = "mongodb+srv://mohitrao83076_db_user:LugF1xwlenkWRE1F@monster.ydmmckl.mongodb.net/?retryWrites=true&w=majority&appName=MONSTER"
+DATABASE_NAME = "monster_bot"
+
+# Panel Configuration
+PANEL_URL = "https://xsilent.shop/vip"
+PANEL_USERNAME = "VIPKEY"
+PANEL_PASSWORD = "roxym830"
+
+# Admin IDs (comma separated for multiple admins)
+ADMIN_IDS = [7192516189]  # Apni ID add kar do
